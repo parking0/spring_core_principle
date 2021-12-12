6480e0ff85a806afde07aa8da74bf2c9be969981
parking0 <p980710@gmail.com>
2021-12-12 23:14:16 +0900

    Change to Spring (1)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,12 +8,15 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration          //설정 정보
 public class AppConfig {            //애플리케이션의 전체 동작 방식을 구성
 
     //AppConfig  --  역할을 세우고 그 안에 구현을 넣어서 => 깔끔하게 만듦 & 중복 제거
     //AppCnfig으로 OrderSerrviceImple과 같은 사용 영역의 어떤 코드도 변경할 필요X
-
+    @Bean               // Spring Container에 등록시키기
     public MemberService memberService(){
         //AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
         /*appConfig 객체는 memoryMemberRepository 객체를 생성하고,
@@ -22,15 +24,18 @@ public class AppConfig {            //애플리케이션의 전체 동작 방식
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {   //AppConfig 리팩터링
+    @Bean
+    public MemberRepository memberRepository() {   //AppConfig 리팩터링
         return new MemoryMemberRepository();                //ctrl+alt+m
     }
 
+    @Bean
     public OrderService orderService(){
         //AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {     //AppConfig 리팩터링
 
         //할인정책을 변경하고 싶을 때 구성 영역만 영향을 받고, 사용 영역은 전혀 영향을 받지 않는다.
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,14 +7,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args){
 
-        AppConfig appConfig = new AppConfig();
+       /*AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();    */
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
 
         /*MemberService memberService = new MemberServiceImpl(null);
         OrderService orderService = new OrderServiceImpl(null,null);*/
